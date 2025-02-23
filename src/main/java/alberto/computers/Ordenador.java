860a635176ab953efcf1e299f7660d5f2358e860
Algama17 <albertogarciamallada@gmail.com>
2025-02-23 22:43:06 +0100

    first code

--- src/main/java/alberto/computers/Ordenador.java
+++ src/main/java/alberto/computers/Ordenador.java
@@ -1,5 +1,21 @@
 package alberto.computers;
 
 public class Ordenador {
+    private String marca;
+    private int memoria;
+    private String procesador;
+    private String sistemaOperativo;
+    private double precio;
 
+    public Ordenador(String marca, int memoria, String procesador, String sistemaOperativo, double precio) {
+        this.marca = marca;
+        this.memoria = memoria;
+        this.procesador = procesador;
+        this.sistemaOperativo = sistemaOperativo;
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
 }
