3dad71c9b6d0a2b61942a60187092c7ec1d157b8
Algama17 <albertogarciamallada@gmail.com>
2025-02-23 22:45:43 +0100

    second test

--- src/test/java/alberto/computers/OrdenadorTest.java
+++ src/test/java/alberto/computers/OrdenadorTest.java
@@ -11,4 +11,10 @@ public class OrdenadorTest {
     void testGetMarca() {
         assertEquals("Dell", ordenador.getMarca(), "La marca del ordenador no coincide.");
     }
+
+    @Test
+    void testToString() {
+        String expected = "Ordenador{marca='Dell', memoria=16GB, procesador='Intel i7', sistemaOperativo='Windows 10', precio=1200.0$}";
+        assertEquals(expected, ordenador.toString(), "La representación del ordenador no es correcta.");
+    }
 }
